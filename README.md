38a11f21084e640b3e7da694073b39ff776c9597
Pat Lowe <lowe-patrick@att.net>
2023-08-30 15:05:28 -0700

    fixed sme bugs and added READme

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
+# ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
 # ORM-E-Commerce-Back-End
+
+## [Patrick Lowe](https://github.com/PatrickWLowe)
+
+## Description
+
+#### An E-Commerce backend using MySQL2
+
+## Technologies-Used
+
+![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)
+
+![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
+
+![MySQL](https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white)
+
+![Sequelize](https://img.shields.io/badge/Sequelize-52B0E7?style=for-the-badge&logo=Sequelize&logoColor=white)
+
+### dotenv
+
+### GitBash
+
+## Installation
+
+#### You will need clone the repository and open the folder locally. Then you will need to run "npm install" in the parent folder. Then you will need to run "npm run seed" Then "npm start" to get the application working. Make GET, PUT, POST, DELETE requests using Insomnia or something simmiliar
+
+## Acceptance Criteria
+
+```md
+GIVEN a functional Express.js API
+WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
+THEN I am able to connect to a database using Sequelize
+WHEN I enter schema and seed commands
+THEN a development database is created and is seeded with test data
+WHEN I enter the command to invoke the application
+THEN my server is started and the Sequelize models are synced to the MySQL database
+WHEN I open API GET routes in Insomnia for categories, products, or tags
+THEN the data for each of these routes is displayed in a formatted JSON
+WHEN I test API POST, PUT, and DELETE routes in Insomnia
+THEN I am able to successfully create, update, and delete data in my database
+```
+
+## License
+
+## This project is licensed under the MIT License
+
+[MIT](https://opensource.org/licenses/MIT)
+
+## Screenshots of the APP
+
+![Deployed Screenshot](./images/ScreenshotORM.png)
+
+## Video Walkthrough
+
+### [Walkthrough](https://drive.google.com/file/d/1lbEBUkZM-9l1PLuSppNQagTmp23WPF_p/view)
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -56,20 +56,10 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// create new product
 router.post("/", (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -78,7 +68,6 @@ router.post("/", (req, res) => {
         });
         return ProductTag.bulkCreate(productTagIdArr);
       }
-      // if no product tags, just respond
       res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
@@ -88,22 +77,18 @@ router.post("/", (req, res) => {
     });
 });
 
-// update product
 router.put("/:id", (req, res) => {
-  // update product data
   Product.update(req.body, {
     where: {
       id: req.params.id,
     },
   })
     .then((product) => {
-      // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
     })
     .then((productTags) => {
-      // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
+
       const newProductTags = req.body.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
@@ -112,12 +97,11 @@ router.put("/:id", (req, res) => {
             tag_id,
           };
         });
-      // figure out which ones to remove
+
       const productTagsToRemove = productTags
         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
         .map(({ id }) => id);
 
-      // run both actions
       return Promise.all([
         ProductTag.destroy({ where: { id: productTagsToRemove } }),
         ProductTag.bulkCreate(newProductTags),
@@ -125,7 +109,6 @@ router.put("/:id", (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
